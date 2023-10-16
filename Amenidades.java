252ec24636a2328c2d944d83e608e7d3cf3e1742
Erick Barrera <dannbj1@gmail.com>
2023-10-16 12:46:48 -0600

    Creación de clases

---
+++ Amenidades.java
@@ -0,0 +1,18 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Departamento de Ciencias de la Computación
+ * Programación Orientada a Objetos
+ * 
+ * clase para almacenar datos de amenidades
+ * 
+ * @author: Erick Barrera - 231238
+ * @version: 1.0.0
+ * @date: 16/10/23
+ * @lastUpdate: 16/10/23
+ * 
+ * Changes: --
+ * 
+ */
+public class Amenidades extends Espacio{
+    
+}
---
+++ Apartamento.java
@@ -0,0 +1,18 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Departamento de Ciencias de la Computación
+ * Programación Orientada a Objetos
+ * 
+ * clase para almacenar datos de apartamentos
+ * 
+ * @author: Erick Barrera - 231238
+ * @version: 1.0.0
+ * @date: 16/10/23
+ * @lastUpdate: 16/10/23
+ * 
+ * Changes: --
+ * 
+ */
+public class Apartamento extends Espacio{
+    
+}
